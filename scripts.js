40b467b08b0a89682f0853997858d9b62a30c7c6
kpargiela <kpargiela7@gmail.com>
2018-02-04 06:31:27 +0100

    Edit

--- scripts.js
+++ scripts.js
@@ -1,5 +1,12 @@
 window.onload = function() {
-  for (var i = 0; i < menuItem.length; i++) {
-    menuItem[i].classList.add('visible');
+
+  let animationDuration = 20000; // 2000 millieseconds
+
+  function animateItems() {
+     menuItem.each(function(i, item) {
+        setTimeout(function(){
+          item.addClass('animate');
+        }, i * animationDuration); // this multiplies the index by the duration so the handler is fired in sequence
+     });
   }
 }();
